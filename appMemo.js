18760b5e810baa1df88d1667b3b634abedde5931
Sylvie-B <unsolo@hotmail.fr>
2021-01-13 21:00:49 +0100

    css work

--- appMemo.js
+++ appMemo.js
@@ -1,2 +1,16 @@
-<!--    change border color of chosen theme    -->
-<!--    change border color of chosen level    -->
+/** listen choices
+ * change border color of chosen theme
+ * change border color of chosen level
+ *
+ * adapt the title
+ * create cards in #choice
+ * create start button in #level
+ *
+ * when start
+ * display :
+ * nbr of cards
+ * try
+ * time
+ * restart
+ */
+
